f66f093c67938eb44bb9c5db3f35cbe2282549a4
DaybsonPaisante <m127391@ipatinga.mg.gov.br>
2024-08-30 12:34:35 -0300

    first commit: console app project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using CadsusServiceReference;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CadsusServicePortTypeClient client;
+
+            client = new CadsusServicePortTypeClient(CadsusServicePortTypeClient.EndpointConfiguration.CadsusServicePort);
+
+            var clientCredentials = client.ClientCredentials;
+            clientCredentials.UserName.UserName = "CADSUS.CNS.PDQ.PUBLICO";
+            clientCredentials.UserName.Password = "kUXNmiiii#RDdlOELdoe00966";
+
+            var result = client.pesquisar(
+                  new requestPesquisar
+                  {
+                      FiltroPesquisa = new FiltroPesquisa
+                      {
+                          CPF = new CPFType
+                          {
+                              numeroCPF = "66105234368"
+                          },
+                          tipoPesquisa = TipoPesquisaType.IDENTICA
+                      }
+                  });
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.NomeCompleto);
+            }
+        }
+    }
+}
